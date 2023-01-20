5966092945215bd716bf78a27d5e11f24cc1758d
sujitmandava <sujitmandava123@gmail.com>
2023-01-20 12:07:06 +0530

    Assignment 1 added+Q1,Q2 implemented

---
+++ Assignment1/Q1.py
@@ -0,0 +1,44 @@
+import matplotlib.pyplot as plt
+from math import log, exp, pi
+
+
+def logStirling(n: int) -> float:
+    # return (n+0.5)*log(n) + 0.5*log(2*pi) - n
+    return ((1 / 2) * (log(2 * pi) + log(n))) + (n * (log(n) - 1))
+
+
+def logFactorial(n: int):
+    lF = []
+    currLog = 0
+    for i in range(1, n+1):
+        currLog += log(i)
+        lF.append(currLog)
+    return lF
+
+
+def factorials(n: int):
+    sF = []  # Approximation
+    logF = logFactorial(n)  # Log of factorial
+    for i in range(1, n+1):
+        sF.append((logStirling(i)))
+    return sF, logF
+
+
+def main():
+    N = 1000000
+    sF, logF = factorials(N)
+    x = list(range(1, N+1))
+    y1 = list(sF)
+    y2 = list(logF)
+    # Plot details
+    plt.title("Visualizing Stirling's Approximation")
+    plt.xlabel("n")
+    plt.ylabel("Logarithm values")
+
+    plt.plot(x, y1, "r", lw=3)
+    plt.plot(x, y2, "black", lw=1.5)
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
---
+++ Assignment1/Q2.py
@@ -0,0 +1,80 @@
+import matplotlib.pyplot as plt
+from random import random
+
+
+class Dice():
+    # Attributes
+    numSides = 6
+    probSides = [1/6, 1/6, 1/6, 1/6, 1/6, 1/6]
+    cdf = []
+
+    def checkProbability(self, pSides):
+        totalProb = 0
+
+        if len(pSides) != self.numSides:
+            raise Exception("Invalid probability distribution")
+
+        for pSide in pSides:
+            totalProb += pSide
+
+        if totalProb != 1:
+            raise Exception("Invalid probability distribution")
+
+    def __init__(self, sides=6) -> None:
+        if (sides < 4):
+            raise Exception("Cannot construct the dice")
+
+        if type(sides) == float:
+            raise Exception("Cannot construct the dice")
+
+        self.numSides = sides
+        pSide = 1/sides
+        pSides = [pSide]*sides
+        self.setProb(pSides)
+
+    def setProb(self, pSides):
+        self.checkProbability(pSides)
+        self.probSides = pSides
+        self.cdf.clear()
+
+        tcdf = 0.0
+        for pSide in pSides:
+            tcdf += pSide
+            self.cdf.append(tcdf)
+
+    def __str__(self):
+        sides = self.numSides
+        pDist = self.probSides
+
+        return "Dice with " + str(sides) + " faces and probability distribution " + str(pDist)
+
+    def roll(self, n):
+        currRolls = [0]*self.numSides
+        for i in range(n):
+            currRoll = random()
+            for p in range(len(self.cdf)):
+                if currRoll < self.cdf[p]:
+                    currRolls[p] += 1
+                    break
+        x = list(range(1, self.numSides+1))
+        yRolls = list(currRolls)
+        yPreds = [i*n for i in self.probSides]
+
+        plt.title(
+            "Outcome of {n} throws of a {numSides}-faced dice".format(
+                n=n, numSides=self.numSides)
+        )
+        plt.xlabel("Sides")
+        plt.ylabel("Occurences")
+        plt.bar([i-0.15 for i in x], yRolls, color="red", width = 0.3)
+        plt.bar([i+0.15 for i in x], yPreds, color="blue", width = 0.3)
+        plt.show()
+
+
+def main():
+    d1 = Dice(12)
+    d1.roll(1000000)
+
+
+if __name__ == "__main__":
+    main()
